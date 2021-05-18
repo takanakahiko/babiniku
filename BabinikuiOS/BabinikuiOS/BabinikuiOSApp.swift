f54ae4f137261451dee3eeb779324930faec8b16
takanakahiko <takanakahiko@gmail.com>
2021-05-18 09:56:30 +0900

    Add BabinikuiOS

---
+++ BabinikuiOS/BabinikuiOS/BabinikuiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabinikuiOSApp.swift
+//  BabinikuiOS
+//
+//  Created by Takayuki Nakayama on 2021/05/18.
+//
+
+import SwiftUI
+
+@main
+struct BabinikuiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabinikuiOS/BabinikuiOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BabinikuiOS
+//
+//  Created by Takayuki Nakayama on 2021/05/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
