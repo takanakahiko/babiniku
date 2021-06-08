485c80fd82cb9e0497bbca0a5e3e70dd361c9c49
takanakahiko <takanakahiko@gmail.com>
2021-06-08 10:54:49 +0900

    Receive UDP data and apply to transform

---
+++ BabinikuUnity/Assets/TransformApplier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using LitJson;
+
+public class TransformApplier : MonoBehaviour
+{
+    private UDPServer _udpServer;
+    public Matrix4x4 matrix;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+        matrix = Matrix4x4.zero;
+        _udpServer = new UDPServer(41234);
+        _udpServer.Received += ReceiveUdp;
+        _udpServer.ListenStart();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Debug.Log(Matrix);
+        // transform.localPosition = new Vector3(matrix.m03, matrix.m13, matrix.m23);
+        var t = transform;
+        t.localRotation = matrix.rotation;
+        t.localScale = matrix.lossyScale;
+    }
+
+    private void ReceiveUdp(string strMsg)
+    {
+        var jsonData = JsonMapper.ToObject(strMsg);
+        var m = Matrix4x4.zero;
+        for (var i = 0; i < 4; i++)
+        {
+            for (var j = 0; j < 4; j++)
+            {
+                m[i,j] = (float)(double)(jsonData["matrix"][i][j]);
+            }
+        }
+        matrix = m;
+    }
+    
+}
---
+++ BabinikuUnity/Assets/UDPServer.cs
@@ -0,0 +1,104 @@
+﻿using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+using System.Net;
+public class UDPServer
+{
+    /// <summary>
+    /// デリゲート 受信時イベント
+    /// </summary>
+    /// <param name="strMsg"></param>
+    public delegate void ReceivedHandler(string strMsg);
+    
+    /// <summary>
+    /// 受信時イベント
+    /// </summary>
+    public ReceivedHandler Received;
+    
+    /// <summary>
+    /// 受信処理 スレッド
+    /// </summary>
+    private Thread _thread;
+    
+    /// <summary>
+    /// リッスンポート
+    /// </summary>
+    private readonly int _nListenPort;
+    
+    /// <summary>
+    /// UDPクライアント
+    /// </summary>
+    private UdpClient _client;
+    
+    //--------------------------------------------------------------------------
+    
+    /// <summary>
+    /// UDPServer
+    /// </summary>
+    public UDPServer(int port = 20000)
+    {
+        _nListenPort = port;
+        _client = null;
+    }
+    
+    /// <summary>
+    /// UDP受信 リッスン開始
+    /// </summary>
+    public void ListenStart()
+    {
+        _client = new UdpClient(_nListenPort);
+        _thread = new Thread(new ThreadStart(Thread));
+        _thread.Start();
+        Debug.Log("UDP Receive thread start");
+    }
+    
+    /// <summary>
+    /// 解放処理
+    /// </summary>
+    public void Dispose()
+    {
+        if(_thread != null)
+        {
+            _thread.Abort();
+            _thread = null;
+        }
+        if(_client != null)
+        {
+            _client.Close();
+            _client.Dispose();
+            _client = null;
+        }
+    }
+    
+    /// <summary>
+    /// スレッド
+    /// </summary>
+    private void Thread()
+    {
+        while (true)
+        {
+            if (_client != null)
+            {
+                try
+                {
+                    IPEndPoint ep = null;
+                    var rcvBytes = _client.Receive(ref ep);
+                    var rcvMsg = System.Text.Encoding.UTF8.GetString(rcvBytes);
+                    if (rcvMsg != string.Empty)
+                    {
+                        //Debug.Log("UDP受信メッセージ : " + rcvMsg);
+                        Received?.Invoke(rcvMsg);
+                    }
+                }
+                catch (System.Exception e)
+                {
+                    Debug.Log(e.Message);
+                }
+            }
+            else
+            {
+                Debug.Log("Error:client = null");
+            }
+        }
+    }
+}
